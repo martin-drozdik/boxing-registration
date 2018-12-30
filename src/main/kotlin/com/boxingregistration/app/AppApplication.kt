ba8af1e5be521f28d3513aa6b849652e3cf97ddf
Martin Drozdik <drozdik.svk@gmail.com>
2018-12-30 18:51:13 +0100

    Added stub application made by initializr.

---
+++ src/main/kotlin/com/boxingregistration/app/AppApplication.kt
@@ -0,0 +1,12 @@
+package com.boxingregistration.app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AppApplication
+
+fun main(args: Array<String>) {
+	runApplication<AppApplication>(*args)
+}
+
