05c01148850ff599ebb45722ac02d52824d1149f
Martin Drozdik <drozdik.svk@gmail.com>
2020-01-21 22:23:48 +0100

    Added footer with icon attribution.

--- src/main/kotlin/com/boxingregistration/app/security/UserDetailsRepository.kt
+++ src/main/kotlin/com/boxingregistration/app/security/UserDetailsRepository.kt
@@ -5,7 +5,6 @@ import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Component
-import org.springframework.stereotype.Repository
 
 @Component
 class UserDetailsRepository(val userRepository: UserRepository): UserDetailsService
